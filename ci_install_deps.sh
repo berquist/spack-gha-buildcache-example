d83b8df29f3af01c96e1944afb24710b23fdfa5f
Eric Berquist <eric.berquist@gmail.com>
2024-01-19 16:39:24 -0500

    complete deps for Red Hat- and Debian-based systems

--- ci_install_deps.sh
+++ ci_install_deps.sh
@@ -3,19 +3,35 @@
 set -e
 set -x
 
-# deps:
-#  - git
-
 if command -v apt-get; then
     apt-get update -y
     apt-get upgrade -y
     apt-get install -y --no-install-recommends \
-            git
-elif command -v yum; then
-    yum update -y
-    yum install -y \
-        git
+            bzip2 \
+            ca-certificates \
+            file \
+            g++ \
+            gcc \
+            git \
+            make \
+            patch \
+            python3 \
+            unzip \
+            xz-utils
+elif command -v dnf; then
+    dnf upgrade -y --allowerasing
+    dnf install -y --allowerasing \
+        bzip2 \
+        file \
+        gcc \
+        gcc-c++ \
+        git \
+        make \
+        patch \
+        unzip \
+        xz
 elif command -v brew; then
+    brew upgrade
     brew install \
          git
 fi
